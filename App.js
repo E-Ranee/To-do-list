75508a79bac0e74de6d9277ac2711421e038e2c3
Esina Ranee <esina.ranee@gmail.com>
2024-03-20 15:16:03 +0000

    first go at input field

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+import Button from './components/Button';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>To do list</Text> 
+        <Text>What needs to be done?</Text>
+        <Text></Text>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add new item here!"
+            onChangeText={newText => setText(newText)}
+            defaultValue={text}
+          />
+          <Button label="Add" />
+        </View>
+
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +39,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: '#ece0ff',
+    fontStyle: "italic",
+  },
+  title: {
+    fontSize: 30,
   },
 });
